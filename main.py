4f4cb7478921b5df7a1959575be65d3a61b13a41
daniel <tishakovdanyilgmail.com>
2024-01-24 21:21:44 +0000

    init commit0

---
+++ main.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+from tkinter import messagebox
+
+class KikTacToe:
+    def __init__(self, window):
+        self.window = window
+        self.window.title("TicTacToe")
+
+    def create_game(self):
+        for i in range(3):
+            for j in range(3):
+                button = tk.Button(self.window, text="", font=('Arial', 50), height=2, width=6, bg="blue",
+                                    command=lambda row=i, col=j: self.handle_click(row, col))
+                button.grid(row=i, col=j, sticky="nsew")
+    
+    def initialize_veriables(self):
+        self.board = [[0,0,0], [0,0,0], [0,0,0]]
+        self.current_player = 1
+
+    
